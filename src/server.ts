d6544f11c5d84533e624973dc3e1ea4a681ded3d
Dylan Vu <dylanvu9@gmail.com>
2024-01-12 22:54:10 -0800

    firestore successfully connected to

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,38 @@
 import express from "express";
 
+// import the service account correctly: https://stackoverflow.com/a/70106896
+import serviceAccount from "../service_account.json";
+
+// import what's needed for the firebase admin module
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { ServiceAccount } from "firebase-admin";
+
+// create the firebase application using the service account
+initializeApp({
+    credential: cert(serviceAccount as ServiceAccount)
+});
+
+// create the firestore database access in the application
+const db = getFirestore();
+
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+// configure some middleware to parse JSON and URL-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    const collection = db.collection("users");
+    const testDoc = collection.doc("test");
+
+    // get the actual data contained in the document
+    const snapshot = await testDoc.get();
+    const data = snapshot.data();
+
+    console.log(data);
+
     res.send("Hello World!");
 });
 
